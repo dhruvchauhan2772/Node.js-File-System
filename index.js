14f6e6ed982b7af18a961303296d938e4985ac57
Dhruv Chauhan <dhruvchauhan.dcs22n@vnsgu.ac.in>
2021-10-09 18:41:47 +0530

    Final Submit

--- index.js
+++ index.js
@@ -1,84 +1,67 @@
-/*
-Write a Node.js program to do the following:
-1.  Create Directory ( Hint: fs.mkdir )
-2.  Remove Directory ( Hint: fs.rmdir )
-3.  Write File 
-4.  Read File 
-5.  Delete File
-6.  Append data to file
-7.  Update / Replace file with new data
-8.  Rename File
-*/
-
-/** Including the built-in process object and readline module */
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-
-/** To include the File System module, use the require() method */
 const fs = require("fs");
-var fileName = "";
-var content = "";
+var filename="";
+var content="";
 
-/** Create File */
-var createFile = () => {
-    fs.writeFile(fileName, content, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("File saved successfully!");
-        }
-        repeat();
-    });
-};
-
-/** Create directory function */
 var createDirWizard = () => {
-    rl.question("Enter name of the directory: ", (ans) => {
+    rl.question("Enter Name For the Directory: ", (ans) => {
         if (!fs.existsSync(ans)) {
             fs.mkdirSync(ans);
         } else {
-            console.log("Soory! This directory is already exist");
+            console.log("Directory is already exist.");
         }
         repeat();
     });
 };
 
-/** Remove directory function */
 var removeDirWizard = () => {
-    rl.question("Enter name of the directory: ", (ans) => {
+    rl.question("Enter Name for remove Directory: ", (ans) => {
         if (fs.existsSync(ans)) {
             fs.rmdirSync(ans);
-            console.log("Diretory is removed successfully");
+            console.log("Diretory is removed.");
         } else {
-            console.log("Soory! This directory is not exist");
+            console.log("Directory is not exist.");
         }
         repeat();
     });
 };
 
-/** Create and write file function */
-var writeFileWizard = () => {
-    rl.question("Enter name of the file: ", (ans) => {
-        fileName = ans + ".txt";
-        rl.question("Enter contect of the file: ", (ans) => {
-            content = ans;
+var createFile=()=>{
+    fs.writeFile(filename,content,(err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("file save successfully.");
+        }
+        repeat();
+    });
+};
+
+var createFileWizard=()=>{
+    console.log("Welcome to file creation wizard.");
+    rl.question("Name of the file",(ans)=>{
+        filename=ans + ".txt";
+        rl.question("Content of the file",(ans)=>{
+            content=ans;
             createFile();
         });
     });
 };
 
-/** Read file function */
-var readFileWizard = () => {
-    rl.question("Enter name of the file: ", (ans) => {
-        fileName = ans + ".txt";
-        fs.readFile(fileName, 'utf8', function(err, data) {
-            if (err) {
-                console.log("File is not exist");
-            } else {
-                console.log("OK :" + fileName);
+var readFileWizard=()=>{
+    rl.question("Enter Name of File:",(ans)=>{
+        filename=ans+".txt";
+        fs.readFile(filename,'utf8',function(err,data){
+            if(err){
+                console.log("File is Not exist.");
+            }
+            else{
+                console.log("Done :" + filename);
                 console.log(data);
             }
             repeat();
@@ -86,30 +69,14 @@ var readFileWizard = () => {
     });
 };
 
-/** Delete file function */
-var deleteFileWizard = () => {
-    rl.question("Enter name of the file: ", (ans) => {
+var appendToFileWizard=()=>{
+    rl.question("Enter Name of File: ", (ans) => {
         fileName = ans + ".txt";
-        fs.unlink(fileName, function(err) {
-            if (err) {
-                console.log("Sorry! File is not exist");
-            } else {
-                console.log('File deleted successfully!');
-            }
-        });
-        repeat();
-    });
-};
-
-/** Appned file function */
-var appendToWizard = () => {
-    rl.question("Enter name of the file: ", (ans) => {
-        fileName = ans + ".txt";
-        rl.question("Enter contect of the file: ", (ans) => {
+        rl.question("Enter Contect of the File: ", (ans) => {
             content = ans;
             fs.appendFile(fileName, content, function(err) {
                 if (err) {
-                    console.log("Sorry! File is not exist");
+                    console.log("File is not exist");
                 } else {
                     console.log('File appended successfully!');
                 }
@@ -119,36 +86,17 @@ var appendToWizard = () => {
     });
 };
 
-/** Update file function */
-var updateFileWizard = () => {
-    rl.question("Enter name of the file: ", (ans) => {
-        fileName = ans + ".txt";
-        rl.question("Enter contect of the file: ", (ans) => {
-            content = ans;
-            fs.writeFile(fileName, content, function(err) {
-                if (err) {
-                    console.log("Sorry! File is not exist");
-                } else {
-                    console.log('File updated successfully!');
-                }
-                repeat();
-            });
-        });
-    });
-};
-
-/** Rename file function */
-var renameFileWizard = () => {
-    rl.question("Enter original file name: ", (ans) => {
+var renameFileWizard=()=>{
+    rl.question("Enter Original File Name: ", (ans) => {
         var replacedFileName = "";
         fileName = ans + ".txt";
-        rl.question("Enter replaced file name: ", (ans) => {
+        rl.question("Enter Name for Replaced File Name: ", (ans) => {
             replacedFileName = ans + ".txt";
             fs.rename(fileName, replacedFileName, (err) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Yuuppyyy! " + fileName + ".txt replaced with " + replacedFileName + ".txt");
+                    console.log("Your File" + fileName + ".txt replaced with " + replacedFileName + ".txt");
                 }
                 repeat();
             });
@@ -156,52 +104,88 @@ var renameFileWizard = () => {
     });
 };
 
-/** Create function for display chocing message to user */
-var instruction = () => {
-    console.log("1.  Create Directory ( Hint: fs.mkdir )");
-    console.log("2.  Remove Directory ( Hint: fs.rmdir )");
-    console.log("3.  Create and write File ");
-    console.log("4.  Read File ");
-    console.log("5.  Delete File");
-    console.log("6.  Append data to file");
-    console.log("7.  Update / Replace file with new data");
-    console.log("8.  Rename File");
-    console.log("9.  Exit");
+var deleteFileWizard=()=>{
+    rl.question("Enter Name for the File: ", (ans) => {
+        fileName = ans + ".txt";
+        fs.unlink(fileName, function(err) {
+            if (err) {
+                console.log("File is not exist.");
+            } else {
+                console.log('File deleted.');
+            }
+        });
+        repeat();
+    });
 };
 
-/** Create function for appropriate choosing */
+var updateFileWizard = () => {
+    rl.question("Enter name for the File: ", (ans) => {
+        fileName = ans + ".txt";
+        rl.question("Enter Contect for the File: ", (ans) => {
+            content = ans;
+            fs.writeFile(fileName, content, function(err) {
+                if (err) {
+                    console.log("File is not exist.");
+                } else {
+                    console.log('File updated.');
+                }
+                repeat();
+            });
+        });
+    });
+};
+
+var instruction=()=>{
+    console.log("Enter 1 to create a new text file.");
+    console.log("Enter 2 to read the contents of a file.");
+    console.log("Enter 3 to append to an existing file.");
+    console.log("Enter 4 to rename of a file.");
+    console.log("Enter 5 to Delete a file.");
+    console.log("Enter 6 to Create a Directry.");
+    console.log("Enter 7 to Delete a Directory.");
+    console.log("Enter 8 to Update a file.");
+    console.log("Enter 0 Exit.");
+};
+
 var start = () => {
-    rl.question("Enter your choice : ", (answer) => {
-        if (answer == "1") {
-            createDirWizard();
-        } else if (answer == "2") {
-            removeDirWizard();
-        } else if (answer == "3") {
-            writeFileWizard();
-        } else if (answer == "4") {
+    rl.question("Enter Your Choice:", (answer) => {
+        if (answer === "1") {
+            createFileWizard();
+        }
+        else if(answer==="2"){
             readFileWizard();
-        } else if (answer == "5") {
-            deleteFileWizard();
-        } else if (answer == "6") {
-            appendToWizard();
-        } else if (answer == "7") {
-            updateFileWizard();
-        } else if (answer == "8") {
+        }
+        else if(answer==="3"){
+            appendToFileWizard();
+        }
+        else if(answer==="4"){
             renameFileWizard();
-        } else if (answer == "9") {
+        }
+        else if(answer==="5"){
+            deleteFileWizard();
+        }
+        else if(answer==="6"){
+            createDirWizard();
+        }
+        else if(answer==="7"){
+            removeDirWizard();
+        }
+        else if(answer==="8"){
+            updateFileWizard();
+        }
+        else if(answer==="0"){
             rl.close();
-        } else {
-            console.log("Sorry! Wrong input. Please select valid choice");
-            repeat();
+        }
+        else{
+            console.log("Wrong Choice! Please try again.");
+            start();
         }
     });
 };
 
-/** Create function for reapeat instructions and choising for users */
-var repeat = () => {
+var repeat=()=>{
     instruction();
     start();
 };
-
-console.log("Welocome to file handling system in Node.js");
+console.log("Welcome to Dhruv File System\n");
 repeat();
